1eae78a090ece6aaab4f911d9b03d02ef170ecc4
HikmeT <hekmt111@gmail.com>
2025-11-22 18:07:56 +0300

    refactor: code refactoring

---
+++ erp-system/ERP.Api/DependencyInjection.cs
@@ -0,0 +1,62 @@
+﻿using ERP.Api.Database;
+using ERP.Api.Middleware;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Newtonsoft.Json.Serialization;
+
+namespace ERP.Api;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddControllers(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+        builder.Services.AddControllers(options =>
+        {
+            options.ReturnHttpNotAcceptable = true;
+        })
+            .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver())
+            .AddXmlSerializerFormatters();
+
+        builder.Services.AddOpenApi();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddErrorHandling(this  WebApplicationBuilder builder)
+    {
+
+        builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+        // HttpContextAccessor for validators
+        builder.Services.AddHttpContextAccessor();
+
+        builder.Services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+            };
+        });
+
+        builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddDatebase(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString("Database"),
+                npgsqlOptions => npgsqlOptions
+                    .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+            .UseSnakeCaseNamingConvention();
+        });
+
+        return builder;
+
+    }
+}
--- erp-system/ERP.Api/Program.cs
+++ erp-system/ERP.Api/Program.cs
@@ -1,3 +1,4 @@
+using ERP.Api;
 using ERP.Api.Database;
 using ERP.Api.Extensions;
 using ERP.Api.Middleware;
@@ -7,44 +8,13 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers(options =>
-{
-    options.ReturnHttpNotAcceptable = true;
-})
-    .AddXmlSerializerFormatters();
-
-// FluentValidation
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
-// HttpContextAccessor for validators
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
-    {
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-    };
-});
-
-builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Database"),
-        npgsqlOptions => npgsqlOptions
-            .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
-    .UseSnakeCaseNamingConvention();
-});
+builder
+    .AddControllers()
+    .AddErrorHandling()
+    .AddDatebase();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
